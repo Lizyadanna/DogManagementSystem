5d64cb51432e89f7b16e4555024fe6731cf04224
Lizy Agbakahi <lizyagbakahi@Lizys-MacBook-Pro.local>
2024-09-30 15:08:54 -0500

    Importation of Scanner Class and Class declaration

---
+++ CostCalc.java
@@ -0,0 +1,11 @@
+
+//Importation of Scanner class
+import java.util.Scanner;
+
+//Declaration of class named CostCalc
+public class CostCalc {
+    // Declaration of main method
+    public static void main(String[] args) throws Exception {
+
+    }
+}
